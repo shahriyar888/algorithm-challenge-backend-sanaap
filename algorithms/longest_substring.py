6f94f53a02cb8b1d64c27ac4ae1537e337aba9e9
xeus <shahriyar.roshanbin@gmail.com>
2025-12-14 12:08:11 +0330

    AI approach (faster result)

--- algorithms/longest_substring.py
+++ algorithms/longest_substring.py
@@ -1,16 +1,16 @@
 from time import time
 
 
-def longest_unique_substring_length(s):
+def longest_unique_substring_length(s: str) -> int:
+    char_set = set()
+    left = 0
     max_length = 0
-    for index, char in enumerate(s):
-        sub = s[index +1 :]
-        while char in sub:
-            sub = sub[:len(sub) -1]
-        max_length = max(max_length,len(sub)+1)
-
-
-
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
     return max_length
 
 
