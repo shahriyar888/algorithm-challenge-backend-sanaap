ce834771ca39e88791cce3825f63ac6e4a93677d
xeus <shahriyar.roshanbin@gmail.com>
2025-12-14 11:52:08 +0330

    first approach

---
+++ algorithms/longest_substring.py
@@ -0,0 +1,15 @@
+def longest_unique_substring_length(s):
+    max_length = 0
+    for index, char in enumerate(s):
+        sub = s[index +1 :]
+        while char in sub:
+            sub = sub[:len(sub) -1]
+        max_length = max(max_length,len(sub)+1)
+
+
+
+    return max_length
+
+
+if __name__ == "__main__":
+    print(longest_unique_substring_length("ABCABCFKAB"))  # 5
